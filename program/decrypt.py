ba4b25b86b764864798e55472ffae99af7424780
Los-had <githubtestefinal@gmail.com>
2021-05-29 16:52:30 -0300

    dando mais opções

--- program/decrypt.py
+++ program/decrypt.py
@@ -3,14 +3,14 @@ from time import sleep
 modo = ''
 resultado = ''
 aviso = 'AVISO: este programa so faz a descriptografia em 3 camadas então aqui vai um exemplo: "a" = "d"'
-aviso2 = 'AVISO: este programa não suporta caracteres especiais'
+aviso2 = 'AVISO: este programa não suporta caracteres especiais e acentos'
 
 print(aviso)
 print(aviso2)
 
 modo = input("1 - Descriptografar\n >  ")
 
-if(modo == '1'):
+if(modo == '1' or 'd' or 'D' or 'Decrypt' or 'decrypt' or 'Descriptografar' or 'descriptografar'):
     texto = input("Insira o texto para descriptografar \n >  ")
  
     for i in range (0, len(texto)):
